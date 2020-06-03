68350d0259483ebfe473f8c894d5dd9a5de4cc5b
Swaroop Hegde <email@swaroophegde.com>
2020-06-03 17:50:06 +0530

    ability to insta invite users and fixed init scripts
    Co-Authored-By: Anomit Ghosh <anomit.ghosh@gmail.com>

--- backend/init_graphdb.py
+++ backend/init_graphdb.py
@@ -11,21 +11,10 @@ driver = GraphDatabase.driver(settings['NEO4J']['URL'], auth=(settings['NEO4J'][
 
 
 def create_test_members(tx):
-    group_secret = 'fe79917f-793e-403a-8c6b-55460d4fb047'
-    u1_uuid = 'c398200b-ec08-4c32-bd09-7854838ba98'
-    u2_uuid = 'f5b413ac-1d1a-46d5-868d-37a06c98eb1e'
-    g_addr = to_normalized_address(eth_account.Account.privateKeyToAccount(keccak(text=group_secret)).address)
-    u1_addr = to_normalized_address(eth_account.Account.privateKeyToAccount(keccak(text=f'{group_secret}{u1_uuid}')).address)
-    u2_addr = to_normalized_address(eth_account.Account.privateKeyToAccount(keccak(text=f'{group_secret}{u2_uuid}')).address)
+    u1 = settings['NEO']
     return tx.run(f"""
-    merge (u3:User {{uuid: '06647970-197f-462f-b2f8-81a705229679', email: 'email@swaroophegde.com', name: 'Swaroop'}})
-    merge (u1:User {{uuid: 'c398200b-ec08-4c32-bd09-7854838ba988', email: 'anomit@blockvigil.com', name: 'Jalal'}})
-    merge (u2:User {{uuid: 'f5b413ac-1d1a-46d5-868d-37a06c98eb1e', email: 'anomit.ghosh@gmail.com', name: 'Anomit'}})
+    merge (u1:User {{uuid: '{u1['uuid']}', email: '{u1['email']}', name: '{u1['name']}'}})
     """)
-
-# 0x497f6332957DbCa1A1947D26bEa710deaD838471
-# 0x2334Ca63a3C79212CDe2Dc8Cdce8256D931FBde1
-
 
 if __name__ == '__main__':
     with driver.session() as session:
--- backend/init_reldb.py
+++ backend/init_reldb.py
@@ -4,68 +4,24 @@ from eth_utils import keccak, to_normalized_address
 import bcrypt
 import time
 from db_session import Session
+from dynaconf import settings
 
 hashed_pw = bcrypt.hashpw('password', bcrypt.gensalt(12))  # 'password' is the password for this user
 
 
 def fill_entries(session):
-    u1 = MoneyVigilUser(
-        uuid='c398200b-ec08-4c32-bd09-7854838ba988',
-        name="Jalal",
-        email='anomit@blockvigil.com',
-        password=hashed_pw,
-        activated=1,
-        activation_token=000000,
-        activated_at=int(time.time()),
-        remaining_invites=1000
-    )
-
-    u2 = MoneyVigilUser(
-        uuid='f5b413ac-1d1a-46d5-868d-37a06c98eb1e',
-        name="Anomit",
-        email='anomit.ghosh@gmail.com',
-        password=hashed_pw,
-        activated=1,
-        activation_token=000000,
-        activated_at=int(time.time()),
-        remaining_invites=1000
-    )
-
-    u3 = MoneyVigilUser(
-        uuid='06647970-197f-462f-b2f8-81a705229679',
-        name="Swaroop",
-        email='email@swaroophegde.com',
-        password=hashed_pw,
-        activated=1,
-        activation_token=000000,
-        activated_at=int(time.time()),
-        remaining_invites=1000
-    )
+    u1 = MoneyVigilUser(**settings['NEO'])
     session.add(u1)
-    session.add(u2)
-    session.add(u3)
     try:
         session.commit()
     except Exception as e:
-        print('Exception adding users', e)
+        print('Exception adding user', e)
     us1 = MoneyVigilUnsubscribeTokens(
         code='b4c2084e-d889-4455-acce-1c3a39658146',
-        user='c398200b-ec08-4c32-bd09-7854838ba988'
-    )
-
-    us2 = MoneyVigilUnsubscribeTokens(
-        code='8bf9538a-f45d-4fa3-8168-adb87712f5bd',
-        user='f5b413ac-1d1a-46d5-868d-37a06c98eb1e'
-    )
-
-    us3 = MoneyVigilUnsubscribeTokens(
-        code='3d3be058-0704-4fe2-87a2-2f0cc8fd59cb',
-        user='06647970-197f-462f-b2f8-81a705229679'
+        user=u1.uuid
     )
 
     session.add(us1)
-    session.add(us2)
-    session.add(us3)
     try:
         session.commit()
     except Exception as e:
